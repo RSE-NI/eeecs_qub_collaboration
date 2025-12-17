cf6a211b3be01fd6790e7d20c986a3054487349e
rse-ni <domhnallcarlin@gmail.com>
2025-12-17 12:15:38 +0000

    creates flexible add_nums.py

---
+++ add_nums.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def add_nums(args):
+	total=0
+	for num in args:
+		total+=int(num)
+	return total
+
+def main():
+	scriptname=sys.argv[0]
+	result = add_nums(sys.argv[1:])
+	print("The script name is:",scriptname)
+	print("Total:",result)
+
+if __name__ == "__main__":
+	main()
